25c02a6736cbbee0a90bbad7d366091c6f951078
mifologic <mifologic@gmail.com>
2022-06-04 19:17:11 +0300

    fix: delete sleep in test

--- src/test/java/tests/ChangeCityTest.java
+++ src/test/java/tests/ChangeCityTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -19,9 +21,8 @@ public class ChangeCityTest extends TestBase {
         step("Открыть главную страницу", () -> open("/"));
         step("Открыть форму выбора города", () -> $("[data-selenium=\"cityButton\"]").click());
         step("Ввести название города", () -> $("[data-selenium=\"top-cities-list\"]").$(byText(city)).click());
-        sleep(2000); // ждём обновления страницы
         step("Проверить, что город изменился", () -> $("[data-selenium=\"cityButton\"]")
                 .should(Condition.text(city))
-                .should(Condition.visible));
+                .should(Condition.visible, Duration.ofSeconds(3)));
     }
 }
